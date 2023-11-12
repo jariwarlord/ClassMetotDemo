ca5bcd3ca33ff1f7838543766230d8788b088dbd
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2023-11-12 15:29:44 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class MusteriManager
+    {
+        Musteri[] musteriler = new Musteri[] {};
+        public void ekle()
+        {
+            Console.WriteLine("Musteri Eklendi...!");
+        }
+        public void sil() 
+        {
+            Console.WriteLine("Müsteri Silindi...!");
+        }
+        public void listele()
+        {
+            Console.WriteLine("Müsteri Listesi...!");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Adı: " + musteri.Ad);
+                Console.WriteLine("SoyAdı: " + musteri.Soyad);
+                Console.WriteLine("Telefonu: " + musteri.Telefon);
+                Console.WriteLine("---------");
+            }
+        }
+    }
+}
